a4c2b18cf8f7c0af2f3c1b405b4aba2764e38149
OleksandrDomin <92186960+OleksandrDomin@users.noreply.github.com>
2023-06-23 00:05:52 +0200

    vers1

---
+++ src/components/contactList/ContactList.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import css from "./ContactList.module.css";
+
+
+
+function ContactList({ contacts, onDeleteContact}) {
+    return (
+        <div >
+        <ul className={css.contactList_list}>
+                {contacts.map(contact => <li className={css.contactList_item}  key={contact.id}> <span >{contact.name}: {contact.number}</span>
+                    <button className={css.button} onClick={()=> onDeleteContact(contact.id)}>Delete</button>
+                </li>
+         )}
+        </ul>
+        </div>
+    )
+};
+
+ContactList.protoType = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+};
+
+export default ContactList;
